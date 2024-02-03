c37db725ef02764c84d0dfa1284a616609c5602f
Nasim Hossain <imnasim3.1415@gmail.com>
2024-02-03 18:07:12 +0600

    Code formatting

--- dishdemocracy/company/models.py
+++ dishdemocracy/company/models.py
@@ -1,28 +1,42 @@
-from django.db import models
+""" 
+    models for the compnay database
+"""
+
 from datetime import date
+from django.db import models
+
 
 # Create your models here.
 class Employee(models.Model):
+    """
+        employee class to store data of employee
+    """
     name = models.CharField(max_length=50, null=True, blank=True)
     username = models.CharField(max_length=50, null=True, blank=True)
     email = models.EmailField(max_length=254, null=True, blank=True)
     password = models.CharField(max_length=50, null=True, blank=True)
     prev_vote_date = models.DateField(default=date(1980, 1, 1), null=True, blank=True)
-        
+
     def __str__(self):
         return self.name
 
 class Restaurant(models.Model):
+    """
+        restaurant class to store data
+    """
     restaurantId=models.IntegerField(blank=True,null=True)
     name = models.CharField(max_length=255,blank=True,null=True)
     username = models.CharField(max_length=255,null=True,blank=True)
     rest_email = models.EmailField(max_length=254,null=True,blank=True)
     rest_password = models.CharField(max_length=50,null=True,blank=True)
-    
+
     def __str__(self):
         return self.name
 
 class Menu(models.Model):
+    """
+        menu class to store data
+    """
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
     items = models.CharField(max_length=255,null=True,blank=True)
     desc=models.CharField(max_length=255,null=True,blank=True)
@@ -31,20 +45,22 @@ class Menu(models.Model):
     def __str__(self):
         return self.items
 
-
-
-
 class Vote(models.Model):
+    """
+        vote class to store data
+    """
     voter = models.ForeignKey(Employee, on_delete=models.CASCADE,null=True)
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE,null=True)
     date=models.DateField(null=True,blank=True)
     def __str__(self):
         return str(self.voter.name)+" "+str(self.restaurant.name)
-    
 
 class Result(models.Model):
+    """
+        result class to store data
+    """
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE,null=True)
     date = models.DateField(null=True,blank=True)
-    
+
     def __str__(self):
         return self.restaurant
--- dishdemocracy/company/views.py
+++ dishdemocracy/company/views.py
@@ -1,108 +1,98 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.shortcuts import render, redirect
-from .forms import RestaurantForm, EmployeeForm, MenuForm
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth import authenticate, login
 from django.contrib import messages
 import datetime
-
 from .models import *
-                    # login(request,authenticate(request,username=loginOwner.username,email=loginOwner.email,password=loginOwner.password))
 
-# Create your views here.
 def home(request):
     if 'employee' in request.POST:
-            checker=Employee.objects.filter(email=request.POST.get('email'))
-            if not checker:
-                messages.error(request,'Account does not exist',extra_tags='painai')
+        checker = Employee.objects.filter(email=request.POST.get('email'))
+        if not checker:
+            messages.error(request, 'Account does not exist', extra_tags='painai')
+        else:
+            print(request.POST.get('email'))
+            employee = Employee.objects.get(email=request.POST.get('email'))
+            password = request.POST.get('password')
+            if employee.password != password:
+                messages.error(request, 'Passwords do not match!', extra_tags='painai')
             else:
-                print(request.POST.get('email'))
-                employee=Employee.objects.get(email=request.POST.get('email'))
-                password=request.POST.get('password')
-                if employee.password!=password:
-                    messages.error(request,'Passwords do not match!',extra_tags='painai')
-                else:
-                   login(request,authenticate(request,username=employee.username,email=employee.email,password=employee.password))
-                   return(redirect('/employee_page'))
+                login(request, authenticate(request, username=employee.username, email=employee.email, password=employee.password))
+                return redirect('/employee_page')
     if 'owner' in request.POST:
-            checker=Restaurant.objects.filter(rest_email=request.POST.get('email'))
-            if not checker:
-                messages.error(request,'Account does not exist',extra_tags='painai')
+        checker = Restaurant.objects.filter(rest_email=request.POST.get('email'))
+        if not checker:
+            messages.error(request, 'Account does not exist', extra_tags='painai')
+        else:
+            print(request.POST.get('email'))
+            owner = Restaurant.objects.get(rest_email=request.POST.get('email'))
+            password = request.POST.get('password')
+            if owner.rest_password != password:
+                messages.error(request, 'Passwords do not match!', extra_tags='painai')
             else:
-                print(request.POST.get('email'))
-                owner=Restaurant.objects.get(rest_email=request.POST.get('email'))
-                password=request.POST.get('password')
-                if owner.rest_password!=password:
-                    messages.error(request,'Passwords do not match!',extra_tags='painai')
-                else:
-                   login(request,authenticate(request,username=owner.username,email=owner.rest_email,password=owner.rest_password))
-                   return(redirect('/restaurant_page'))
-            
+                login(request, authenticate(request, username=owner.username, email=owner.rest_email, password=owner.rest_password))
+                return redirect('/restaurant_page')
+
     return render(request, 'home.html')
 
-
-
-
-
 def employee_page(request):
-    employee=Employee.objects.get(email=request.user.email)
-    restaurantList=Restaurant.objects.filter()
-    today=datetime.datetime.today()
-    checker=0
-    votedList=Vote.objects.filter(voter=employee,date=today)
+    employee = Employee.objects.get(email=request.user.email)
+    restaurantList = Restaurant.objects.filter()
+    today = datetime.datetime.today()
+    checker = 0
+    votedList = Vote.objects.filter(voter=employee, date=today)
     if votedList:
-        checker=1
-    queryDict={}
+        checker = 1
+    queryDict = {}
     for o in restaurantList:
-        menuList=Menu.objects.filter(restaurant=o)
-        queryDict[o]=menuList
+        menuList = Menu.objects.filter(restaurant=o)
+        queryDict[o] = menuList
     if 'vote' in request.POST:
-        restaurant=Restaurant.objects.get(restaurantId=int(request.POST.get('vote')))
-        vote=Vote(
+        restaurant = Restaurant.objects.get(restaurantId=int(request.POST.get('vote')))
+        vote = Vote(
             restaurant=restaurant,
             voter=employee,
             date=today
         )
         vote.save()
         return redirect('/employee_page')
-        
-    cont={
-        'queryDict':queryDict,
-        'checker':checker
+
+    cont = {
+        'queryDict': queryDict,
+        'checker': checker
     }
-    return render(request, 'employee_page.html',cont)
+    return render(request, 'employee_page.html', cont)
 
 def winner(request):
-    today=datetime.datetime.today()
-    queryDict={}
-    restaurantList=Vote.objects.filter(date=today)
+    today = datetime.datetime.today()
+    queryDict = {}
+    restaurantList = Vote.objects.filter(date=today)
     for o in restaurantList:
-        queryDict[o.restaurant.restaurantId]=0
+        queryDict[o.restaurant.restaurantId] = 0
     for o in restaurantList:
-        queryDict[o.restaurant.restaurantId]+=1
-    sortedList=sorted(queryDict.items(), key=lambda x: x[1], reverse=True)
-    winnerRestaurant=sortedList[0][0]
-    temp=restaurantList[0].restaurant.restaurantId
-    
-    finalDict={}
+        queryDict[o.restaurant.restaurantId] += 1
+    sortedList = sorted(queryDict.items(), key=lambda x: x[1], reverse=True)
+    winnerRestaurant = sortedList[0][0]
+    temp = restaurantList[0].restaurant.restaurantId
+
+    finalDict = {}
     for o in sortedList:
-        rest=Restaurant.objects.get(restaurantId=o[0])
-        finalDict[rest]=o[1]
+        rest = Restaurant.objects.get(restaurantId=o[0])
+        finalDict[rest] = o[1]
     print(winnerRestaurant)
-    cont={
-        'today':today,
-        'finalDict':finalDict,
-        'winner':winnerRestaurant
+    cont = {
+        'today': today,
+        'finalDict': finalDict,
+        'winner': winnerRestaurant
     }
-    return render(request,'winner.html',cont)
+    return render(request, 'winner.html', cont)
+
 def restaurant_page(request):
-    user=request.user
-    restaurant=Restaurant.objects.get(rest_email=user.email)
-    menuList=Menu.objects.filter(restaurant=restaurant)
+    user = request.user
+    restaurant = Restaurant.objects.get(rest_email=user.email)
+    menuList = Menu.objects.filter(restaurant=restaurant)
     if 'add' in request.POST:
-        addMenu=Menu(
+        addMenu = Menu(
             restaurant=restaurant,
             items=request.POST.get('name'),
             image=request.FILES.get('image'),
@@ -110,41 +100,43 @@ def restaurant_page(request):
         )
         addMenu.save()
         return redirect('/restaurant_page')
-    cont={
-        'menuList':menuList
+    cont = {
+        'menuList': menuList
     }
-    return render(request, 'restaurant_page.html',cont)
+    return render(request, 'restaurant_page.html', cont)
+
 def register(request):
     if 'owner' in request.POST:
-                 ownerEmail=request.POST.get('email')
-                 ownerPassword=request.POST.get('password')
-                 ownerUsername=request.POST.get('username')
-                 restName=request.POST.get('restName')
-                 size=len(Restaurant.objects.filter())+1
-                 createRestaurant=Restaurant(
-                     restaurantId=size,
-                     rest_email=ownerEmail,
-                     rest_password=ownerPassword,
-                     username=ownerUsername,
-                     name=restName
-                 )
-                 createRestaurant.save()
-                 user=User.objects.create_user(username=request.POST.get('username'),email=request.POST.get('email'),password=request.POST.get('password'))
-                 return redirect('/')
-    return render(request,'register_restaurant.html')
+        ownerEmail = request.POST.get('email')
+        ownerPassword = request.POST.get('password')
+        ownerUsername = request.POST.get('username')
+        restName = request.POST.get('restName')
+        size = len(Restaurant.objects.filter()) + 1
+        createRestaurant = Restaurant(
+            restaurantId=size,
+            rest_email=ownerEmail,
+            rest_password=ownerPassword,
+            username=ownerUsername,
+            name=restName
+        )
+        createRestaurant.save()
+        user = User.objects.create_user(username=request.POST.get('username'), email=request.POST.get('email'), password=request.POST.get('password'))
+        return redirect('/')
+    return render(request, 'register_restaurant.html')
+
 def registerEmployee(request):
     if 'employee' in request.POST:
-                 name=request.POST.get('name')
-                 employeeEmail=request.POST.get('email')
-                 employeePassword=request.POST.get('password')
-                 employeeUsername=request.POST.get('username')
-                 createEmployee=Employee(
-                     name=name,
-                     email=employeeEmail,
-                     password=employeePassword,
-                     username=employeeUsername,
-                 )
-                 createEmployee.save()
-                 user=User.objects.create_user(username=request.POST.get('username'),email=request.POST.get('email'),password=request.POST.get('password'))
-                 return redirect('/')
-    return render(request,'register_employee.html')
+        name = request.POST.get('name')
+        employeeEmail = request.POST.get('email')
+        employeePassword = request.POST.get('password')
+        employeeUsername = request.POST.get('username')
+        createEmployee = Employee(
+            name=name,
+            email=employeeEmail,
+            password=employeePassword,
+            username=employeeUsername,
+        )
+        createEmployee.save()
+        user = User.objects.create_user(username=request.POST.get('username'), email=request.POST.get('email'), password=request.POST.get('password'))
+        return redirect('/')
+    return render(request, 'register_employee.html')
